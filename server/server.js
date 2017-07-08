5d34f7a08b1621b63970daec077f8a7d7bf4d831
Steven Simonitch <steven.simonitch@tastemade.com>
2017-07-08 09:37:52 -0700

    new user emit and broadcast

--- server/server.js
+++ server/server.js
@@ -19,13 +19,29 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New user connected');
 
+  socket.emit('newMessage', {
+    from: 'Admin',
+    text: 'Welcome to the chat app',
+    createdAt: new Date().getTime()
+  });
+
+  socket.broadcast.emit('newMessage', {
+    from: 'Admin',
+    text: 'new user joined',
+    createdAt: new Date().getTime()
+  });
+
   socket.on('createMessage', (message) => {
-    console.log(`createdMessage ${JSON.stringify(message, null, 2)}`);
     io.emit('newMessage', {
       from: message.from,
       text: message.text,
       createdAt: new Date().getTime()
     });
+    // socket.broadcast.emit('newMessage', {
+    //   from: message.from,
+    //   text: message.text,
+    //   createdAt: new Date().getTime()
+    // });
   });
 
   socket.on('disconnect', () => {
