69c946b1b7af19abd52e309460cc54ba66108e50
Steven Simonitch <steven.simonitch@tastemade.com>
2017-07-07 20:51:38 -0700

    set up websocket

--- server/server.js
+++ server/server.js
@@ -1,15 +1,29 @@
 require('./config/config');
 
+const http = require('http');
 const path = require('path');
 const express = require('express');
+const socketIO = require('socket.io');
 
 const app = express();
 const port = process.env.PORT;
+const server = http.createServer(app);
 const publicPath = path.join(__dirname, '../public');
+
+// create websocket server
+const io = socketIO(server);
 
 // server static file
 app.use(express.static(publicPath));
 
-app.listen(port, () => {
+io.on('connection', (socket) => {
+  console.log('New user connected');
+
+  socket.on('disconnect', () => {
+    console.log('Client disconnected');
+  });
+});
+
+server.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
