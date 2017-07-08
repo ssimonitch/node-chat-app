dd31d25b50164b43b6f5eac1e9a87933e809d5b0
Steven Simonitch <steven.simonitch@tastemade.com>
2017-07-08 11:48:43 -0700

    styling

--- public/js/index.js
+++ public/js/index.js
@@ -27,29 +27,34 @@ socket.on('newLocationMessage', function(message) {
 
 $('#message-form').on('submit', function(e) {
   e.preventDefault();
+
+  var messageTextBox = $('[name=message]');
+
   socket.emit('createMessage', {
     from: 'User',
-    text: $('[name=message]').val()
-    // acknowledgement callback
+    text: messageTextBox.val()
   }, function() {
-
+    messageTextBox.val('');
   });
 });
 
 var locationButton = $('#send-location');
 
 locationButton.on('click', function() {
-
   if (!navigator.geolocation) {
     return alert('Geolocation not supported by your browser.');
   }
 
+  locationButton.attr('disabled', 'disabled').text('Sending location');
+
   navigator.geolocation.getCurrentPosition(function (position) {
+    locationButton.removeAttr('disabled').text('Send location');
     socket.emit('createLocationMessage', {
       latitude: position.coords.latitude,
       longitude: position.coords.longitude
     });
   }, function() {
+    locationButton.removeAttr('disabled').text('Send location');
     alert('Unable to fetch location');
   });
 });
